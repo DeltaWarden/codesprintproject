2c137b9263568e72798c30a5cc6b0c9d983e30bc
DeltaWarden <deltayt.ro@gmail.com>
2023-12-11 08:38:32 +0300

    1.4

---
+++ site/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import sqlite3
+
+@app.route('/authorization', methods=['GET', 'POST'])
+def form_authorization():
+   if request.method == 'POST':
+       Login = request.form.get('Login')
+       Password = request.form.get('Password')
+
+      db_lp = sqlite3.connect('login_password.db')
+       cursor_db = db_lp.cursor()
+       cursor_db.execute(('''SELECT password FROM passwords
+                                               WHERE login = '{}';
+                                               ''').format(Login))
+       pas = cursor_db.fetchall()
+
+       cursor_db.close()
+       try:
+           if pas[0][0] != Password:
+               return render_template('auth_bad.html')
+       except:
+           return render_template('auth_bad.html')
+
+       db_lp.close()
+       return render_template('successfulauth.html')
+
+   return render_template('authorization.html')
