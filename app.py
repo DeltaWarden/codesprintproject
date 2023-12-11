5d071689a8080760ff98f52894005e4373ad4142
Georgy <101892451+goxa2020@users.noreply.github.com>
2023-12-11 11:50:39 +0300

    1.5

--- app.py
+++ app.py
@@ -1,54 +1,83 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import sqlite3
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def reg():
+    return render_template('myprojects.html')
+
+
+@app.route('/myprojects')
+def index():
+    return render_template('myprojects.html')
+
+
+@app.route('/chats')
+def chats():
+    return render_template('chats.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/mail')
+def mail():
+    return render_template('mail.html')
+
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
+
+
 @app.route('/authorization', methods=['GET', 'POST'])
 def form_authorization():
-   if request.method == 'POST':
-       Login = request.form.get('Login')
-       Password = request.form.get('Password')
+    if request.method == 'POST':
+        login = request.form.get('login')
+        Password = request.form.get('Password')
 
-       db_lp = sqlite3.connect('login_password.db')
-       cursor_db = db_lp.cursor()
-       cursor_db.execute(('''SELECT password FROM passwords
-                                               WHERE login = '{}';
-                                               ''').format(Login))
-       pas = cursor_db.fetchall()
+        db_lp = sqlite3.connect('login_password.db')
+        cursor_db = db_lp.cursor()
+        cursor_db.execute(f'SELECT password FROM passwords WHERE login = "{login}";')
+        pas = cursor_db.fetchall()
 
-       cursor_db.close()
-       try:
-           if pas[0][0] != Password:
-               return render_template('auth_bad.html')
-       except:
-           return render_template('auth_bad.html')
+        cursor_db.close()
+        try:
+            if pas[0][0] != Password:
+                return render_template('auth_bad.html')
+        except Exception:
+            return render_template('auth_bad.html')
 
-       db_lp.close()
-       return render_template('successfulauth.html')
+        db_lp.close()
+        return render_template('successfulauth.html')
 
-   return render_template('authorization.html')
+    return render_template('login.html')
+
 
 @app.route('/registration', methods=['GET', 'POST'])
 def form_registration():
+    if request.method == 'POST':
+        Login = request.form.get('Login')
+        Password = request.form.get('Password')
 
-   if request.method == 'POST':
-       Login = request.form.get('Login')
-       Password = request.form.get('Password')
+        db_lp = sqlite3.connect('login_password.db')
+        cursor_db = db_lp.cursor()
+        sql_insert = '''INSERT INTO passwords VALUES('{}','{}');'''.format(Login, Password)
+        cursor_db.execute(sql_insert)
 
-       db_lp = sqlite3.connect('login_password.db')
-       cursor_db = db_lp.cursor()
-       sql_insert = '''INSERT INTO passwords VALUES('{}','{}');'''.format(Login, Password)
-       cursor_db.execute(sql_insert)
+        cursor_db.close()
 
-       cursor_db.close()
+        db_lp.commit()
+        db_lp.close()
 
-       db_lp.commit()
-       db_lp.close()
+        return render_template('successfulregis.html')
 
-       return render_template('successfulregis.html')
-
-   return render_template('registration.html')
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
- app.run()
+    app.run(debug=True)
---
+++ static/js/support.js
@@ -0,0 +1,28 @@
+/*404 - не найдено:( 
+    
+---\ Свитчер для тем*/
+const toggle = document.getElementById("toggle");
+const theme = window.localStorage.getItem("theme");
+
+if (theme === "dark") document.body.classList.add("dark");
+
+toggle.addEventListener("click", () => {
+   document.body.classList.toggle("dark");
+   if (theme === "dark") {
+     window.localStorage.setItem("theme", "light");
+   } else window.localStorage.setItem("theme", "dark");
+});
+
+function chatsf() {
+    window.location.href = 'chats';
+}
+
+function mailf() {
+    window.location.href = 'mail';
+}
+
+function myprojectsf() {
+    window.location.href = 'myprojects';
+}
+
+
